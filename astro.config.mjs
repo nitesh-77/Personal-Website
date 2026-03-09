728a4b15fff6799f1929edb49aa0d32356a5a37c
nitesh-77 <nitesh33lol@gmail.com>
2026-03-09 23:32:28 +0530

    configure github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     site: 'https://nitesh-77.github.io',
-    base: '/Personal-Website',
     vite: {
         plugins: [tailwindcss()],
     },
